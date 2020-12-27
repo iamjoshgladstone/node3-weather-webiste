be942be9b85e0f2b1adb7e2fa51c2a0685bf90f2
Joshua Gladstone <joshgladstone@Joshuas-MacBook-Pro.local>
2020-12-27 12:38:55 -0700

    This is a test commit

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,7 @@ const messageTwo = document.querySelector('#message-2')
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
+    console.log('Hey John!')
     const location = search.value
     messageOne.textContent='Loading...'
     messageTwo.textContent=''
